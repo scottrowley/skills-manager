34f2710b6da270560f3455aa0aa2bb8522adab03
Scott Rowley <srowley@amplience.com>
2020-06-28 17:24:53 +0100

    Initial project bootstrapping

---
+++ src/main/java/net/scottrowley/skillsmanager/SkillsManagerApplication.java
@@ -0,0 +1,12 @@
+package net.scottrowley.skillsmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkillsManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkillsManagerApplication.class, args);
+	}
+}
---
+++ src/test/java/net/scottrowley/skillsmanager/SkillsManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package net.scottrowley.skillsmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkillsManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
